b17ed821c2ab296c64da14e102483f0e489eeac8
MarinaPaley <marina_paley@mail.ru>
2024-11-12 20:18:59 +0300

    не получается

--- Domain/Author.cs
+++ Domain/Author.cs
@@ -10,6 +10,13 @@ namespace Domain
     /// </summary>
     public sealed class Author : IEquatable<Author>
     {
+        [Obsolete("For ORM only")]
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Рассмотрите возможность добавления модификатора "required" или объявления значения, допускающего значение NULL.
+        private Author()
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Рассмотрите возможность добавления модификатора "required" или объявления значения, допускающего значение NULL.
+        {
+        }
+
         /// <summary>
         /// Инициализирует новый экземпляр класса <see cref="Author"/>.
         /// </summary>
